4416839bee43fbee3859d33d0d3849c4dc4a6607
Zanion <dkerns0123@gmail.com>
2013-04-24 01:46:37 -0700

    Add all files to git

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Disable_Internet_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private network _networkConnection = new network();
+        private System.Windows.Forms.NotifyIcon m_notifyIcon;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            m_notifyIcon = new System.Windows.Forms.NotifyIcon();
+            m_notifyIcon.BalloonTipText = "Ethernet Assassin has been minimised. Click the tray icon to restore.";
+            m_notifyIcon.BalloonTipTitle = "Ethernet Assassin";
+            m_notifyIcon.Text = "Ethernet Assassin";
+            m_notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly().Location);
+            m_notifyIcon.Click += new EventHandler(m_notifyIcon_Click);
+        }
+
+        private void EnableButton_Click(object sender, RoutedEventArgs e)
+        {
+            _networkConnection.ToggleInternet(true, NetworkTextBox.Text);
+        }
+
+        private void DisableButton_Click(object sender, RoutedEventArgs e)
+        {
+            _networkConnection.ToggleInternet(false, NetworkTextBox.Text);
+        }
+
+        void OnClose(object sender, CancelEventArgs args)
+        {
+            m_notifyIcon.Dispose();
+            m_notifyIcon = null;
+        }
+
+        private WindowState m_storedWindowState = WindowState.Normal;
+
+        void OnStateChanged(object sender, EventArgs args)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                this.Visibility = Visibility.Collapsed;
+                if (m_notifyIcon != null)
+                    m_notifyIcon.ShowBalloonTip(2000);
+            }
+            else
+                m_storedWindowState = WindowState;
+        }
+        void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            CheckTrayIcon();
+        }
+
+        void m_notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+            Show();
+            WindowState = m_storedWindowState;
+        }
+        void CheckTrayIcon()
+        {
+            ShowTrayIcon(!IsVisible);
+        }
+
+        void ShowTrayIcon(bool show)
+        {
+            if (m_notifyIcon != null)
+                m_notifyIcon.Visible = show;
+        }
+
+    }
+}
---
+++ network.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disable_Internet_Application
+{
+    class network
+    {
+
+        public void ToggleInternet(bool enabled, string networkName)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            string state = (enabled) ? "enabled" : "disabled";
+            startInfo.Arguments = string.Format("/C netsh interface set interface {0} {1}", networkName, state);
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+    }
+}
